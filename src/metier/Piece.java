2cbbd0a863a722b091c47395577cd50ad4261781
Hugo VICENTE <hugovicente965@gmail.com>
2023-03-28 08:55:12 +0200

    création de la classe Piece

--- src/metier/Piece.java
+++ src/metier/Piece.java
@@ -2,5 +2,19 @@ package metier;
 
 public abstract class Piece
 {
+	public Piece()
+	{
 
+	}
+
+	public void deplacer(int col, int lig)
+	{
+		if (this.peutDeplacer(lig, col))
+		{
+			this.lig = lig;
+			this.col = col;
+		}
+	}
+
+	public abstract boolean peutDeplacer(int lig, int col);
 }
