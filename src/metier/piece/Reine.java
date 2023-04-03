ec52d421304472763812ce4ceebb36f0e462f73f
Hugo VICENTE <hugovicente965@gmail.com>
2023-04-03 11:53:03 +0200

    last modif

--- src/metier/piece/Reine.java
+++ src/metier/piece/Reine.java
@@ -34,25 +34,25 @@ public class Reine extends Piece
 
 		        //vérifie si le déplacement, correspond à celui de la tour 
 		return (this.getLig() == ligDest && this.getCol() != colDest || this.getCol() == colDest && this.getLig() != ligDest && 
-		        (!this.autresPieces(Piece.metier.getLstPiece(), ligDest, colDest) ||
+		        	(!this.autresPieces(Piece.metier.getLstPiece(), ligDest, colDest) ||
 		
-		        //vérifie si une pièce est dans une des cardinalités adjacente
-		        (this.getLig() + 1 == ligDest || this.getLig() - 1 == ligDest   || 
-		         this.getCol() + 1 == colDest || this.getCol() - 1 == colDest)) ||
+		         //vérifie si une pièce est dans une des cardinalités adjacente
+		        		( this.getLig() + 1 == ligDest || this.getLig() - 1 == ligDest ||
+		        		  this.getCol() + 1 == colDest || this.getCol() - 1 == colDest    ) ) ||
 
-		       //vérifie si le déplacement, correspond à celui du fou
-		         Math.abs(this.getLig() - ligDest) == Math.abs(this.getCol() - colDest) &&
-		        (!this.autresPieces(Piece.metier.getLstPiece(), ligDest, colDest)       ||
+		        //vérifie si le déplacement, correspond à celui du fou
+		        Math.abs(this.getLig() - ligDest) == Math.abs(this.getCol() - colDest)        &&
+		         	(!this.autresPieces(Piece.metier.getLstPiece(), ligDest, colDest)    ||
 
-		        //verifi si une pièce est dans la diagonale
-		        (this.getLig() + 1 == ligDest && this.getCol() + 1 == colDest ||
-		         this.getLig() - 1 == ligDest && this.getCol() - 1 == colDest ||
-		         this.getLig() - 1 == ligDest && this.getCol() + 1 == colDest ||
-		         this.getLig() + 1 == ligDest && this.getCol() - 1 == colDest))
+		         //verifie si une pièce est dans la diagonale
+		        		(this.getLig() + 1 == ligDest && this.getCol() + 1 == colDest ||
+		        		 this.getLig() - 1 == ligDest && this.getCol() - 1 == colDest ||
+		        		 this.getLig() - 1 == ligDest && this.getCol() + 1 == colDest ||
+		        		 this.getLig() + 1 == ligDest && this.getCol() - 1 == colDest   ) )
 		       )
-				&&
-				(this.mange(ligDest, colDest, Piece.metier.getLstPiece()) != null &&
-				!this.autresPieces(Piece.metier.getLstPiece(), ligDest, colDest));
+			   &&
+			   ( this.mange(ligDest, colDest, Piece.metier.getLstPiece()) != null &&
+				!this.autresPieces(Piece.metier.getLstPiece(), ligDest, colDest)    )     ;
 	}
 
 	public char getSymbole(){return 'Q';}
